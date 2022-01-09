b6d756320347899b2f5df8fd70b507c254398251
meh430 <mehulpillai30@gmail.com>
2022-01-09 15:33:13 -0500

    Sets up datasource

---
+++ app/src/main/java/com/example/redditwalls/datasources/HistoryDAO.kt
@@ -0,0 +1,26 @@
+package com.example.redditwalls.datasources
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.redditwalls.models.History
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface HistoryDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertHistory(history: History)
+
+    @Query("DELETE FROM History")
+    suspend fun deleteHistory()
+
+    @Query("DELETE FROM History WHERE id = :id")
+    suspend fun deleteHistoryItem(id: Long)
+
+    @Query("SELECT * FROM History ORDER BY dateCreated")
+    fun getHistoryFlow(): Flow<List<History>>
+
+    @Query("SELECT * FROM History")
+    suspend fun getHistory(): List<History>
+}
